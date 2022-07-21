33a1bd71ff9086316146772a85db4732064fe9ca
Andrej Palic <andrejpalic@gmail.com>
2022-07-21 20:45:02 +0200

    Add "getRand" function

--- main.js
+++ main.js
@@ -1,7 +1,11 @@
+function getRand(min, max) {
+	return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
 btnNewSticky.addEventListener('click', onNewSticky);
 
 function onNewSticky() {
-//console.log('Hi')
+	//console.log(getRand(1, 3))
 	let stickyNew = document.createElement('div');
 	stickyNew.classList.add('sticky');
 	document.body.appendChild(stickyNew);
